d85cf6faed3d14754800653ef184ffca53563afc
Aung Khant Min <aungkhantmin2014@gmail.com>
2022-01-02 12:06:36 -0500

    minor wording change

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -5,7 +5,7 @@ function Home () {
         <div style={{flexDirection: 'column', textAlign: 'center'}}>
             <h1>Welcome to Algorithm Visualizer</h1>
                     <h2>Enter the array (for example, 3,4,5,2,1). Press Submit.</h2>
-            <h2>Use the Next and Previous buttons to go through the steps</h2>
+            <h2>Use the Next and Previous buttons to go through the steps.</h2>
         </div>
 
     );
